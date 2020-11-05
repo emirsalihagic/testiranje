b7aecebc6fa9c0470bdf45916439251077def9da
emirsalihagic <e.salihagic@gmail.com>
2020-11-05 19:33:21 +0100

    emir 2

---
+++ Test Suites/TE_regresija.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TE_regresija</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>01ec0177-547d-48af-95ef-c0372f4eebc6</testSuiteGuid>
+   <testCaseLink>
+      <guid>87a66a47-67ba-4fea-83b9-d96c484ae59f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test_case_2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
